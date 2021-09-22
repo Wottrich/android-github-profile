ab2cd04a9ed103f94357f3e7c117cbdb7ec941ae
Lucas Cruz Wottrich <wottrich78@gmail.com>
2021-09-22 19:07:08 -0300

    Adiciona buildSrc para gerenciar versões e paths das bibliotecas utilizadas no projeto (#10)
    * feat: update .gitignore and remove .idea file
    
    * feat: add buildSrc to manage libraries' version and path

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,42 @@
+import org.gradle.api.JavaVersion
+
+object Versions {
+
+    const val kotlinVersion = "1.5.21"
+    const val coroutines = "1.3.9"
+    const val coreKtxVersion = "1.6.0"
+
+    //AppCompat and UI things
+    const val appCompatVersion = "1.4.0-alpha01"
+    const val material = "1.4.0"
+    const val constraintLayout = "2.0.4"
+    const val recyclerView = "1.2.0-alpha05"
+
+    //Android
+    const val lifecycle = "2.2.0"
+    const val activityVersion = "1.2.3"
+    const val livedataLifecycle = "2.4.0-alpha01"
+    const val lifecycleRuntime = "2.4.0-alpha01"
+
+    //Koin
+    const val koinVersion = "3.1.2"
+
+    //Compose
+    const val composeVersion = "1.0.2"
+    const val composeActivityVersion = "1.3.1"
+
+    //Coil
+    const val coilVersion = "1.3.0"
+
+    //Retrofit and api things
+    const val retrofitVersion = "2.9.0"
+    const val loggingInterceptorVersion = "4.9.0"
+
+    //Gradle
+    const val gradleVersion = "7.0.0"
+
+    //Test
+    const val junitVersion = "4.13.2"
+    const val junitExtVersion = "1.1.3-beta01"
+    const val espressoCoreVersion = "3.4.0-beta01"
+}
